15ce4c6164f33a7f6e355bcd20be6f8f38a7d9c4
liveenergy7-code <liveenergy7@gmail.com>
2026-07-19 20:45:14 -0300

    feat(arquitetura): implementa Fase 2 - Adaptador2026 e MotorAnaliticoV2 seguindo o principio de coexistencia

--- Dominio/Metamodelos.js
+++ Dominio/Metamodelos.js
@@ -5,8 +5,14 @@
  * as capacidades de cada versão e sua cobertura histórica (o que era gravado 
  * e o que não existia na época).
  */
+const FonteDados = {
+  OPP_2026: "OPP_2026",
+  OPP_2025: "OPP_2025",
+  FIREBASE_OCORRENCIAS: "FIREBASE_OCORRENCIAS"
+};
+
 const CatalogoEstruturas = {
-  "OPP_2026": {
+  [FonteDados.OPP_2026]: {
     versao: "2026",
     adaptador: "Adaptador2026", // Nome da classe/objeto que processará os dados
     camposObrigatorios: ["MATRICULA", "DATA", "MIKE"],
@@ -25,7 +31,7 @@ const CatalogoEstruturas = {
       boc: true
     }
   },
-  "OPP_2025": {
+  [FonteDados.OPP_2025]: {
     versao: "2025",
     adaptador: "Adaptador2025",
     camposObrigatorios: ["MATRICULA", "DATA", "MIKE"],
